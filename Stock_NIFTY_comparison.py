f2a38e08cda6d6364f0f40422f5fe6218bd78fc4
Abhinav Mathur <abhinav.mathur50@gmail.com>
2021-11-14 15:41:30 +0530

    Update Stock_NIFTY_comparison.py

--- Stock_NIFTY_comparison.py
+++ Stock_NIFTY_comparison.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
 from datetime import date
 from nsepy import get_history
 
@@ -16,6 +17,27 @@ elif end_dt <= start_dt:
 	st.text('Please select appropriate date ranges')
 else:
 	st.text('Showing analysis of '+stock+' vs NIFTY for '+str(start_dt)+' to '+str(end_dt))
+
+
+# Get NIFTY Data
+
+nifty_df = get_history(symbol = 'NIFTYBEES', start = start_dt, end = end_dt)
+
+if nifty_df.shape[0] == 0:
+	st.text("Unable to download NIFTY data please retry")
+
+nifty_df = nifty_df['symbol','VMAP','Open','High','Low']
+
+stock_df = get_history(symbol = stock, start = start_dt, end = end_dt)
+
+if stock_df.shape[0] == 0:
+	st.text('Unable to get stock data, please retry or rework on the input parameters')
+
+stock_df = stock_df['symbol','VMAP','Open','High','Low']
+stock_df.columns = ['stock'+var for var in stock_df.columns]
+
+analysis_pd = pd.merge(stock_df, nifty_df, on = 'Date')
+st.text('Done!')
 
 
 # Functionalities to show
